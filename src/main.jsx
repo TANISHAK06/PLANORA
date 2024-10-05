8d802b6facabe0bdd2ce4505057e5adc21427c91
TANISHAK06 <tanishakshukla08@gmail.com>
2024-10-06 03:46:58 +0530

    huhue

--- src/main.jsx
+++ src/main.jsx
@@ -5,20 +5,33 @@ import "./index.css";
 import CreateTrip from "./create-trip/index.jsx";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Header from "./components/ui/custom/Header.jsx";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "create-trip",
-    element: <CreateTrip />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//   },
+//   {
+//     path: "create-trip",
+//     element: <CreateTrip />,
+//   },
+// ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Header />
-    <RouterProvider router={router} basename="/PLANORA" />
+    {/* <RouterProvider router={router} basename="/PLANORA" /> */}
+    <RouterProvider
+      router={createBrowserRouter([
+        {
+          path: "/",
+          element: <App />,
+        },
+        {
+          path: "create-trip",
+          element: <CreateTrip />,
+        },
+      ])}
+      basename="/PLANORA"
+    />
   </StrictMode>
 );
