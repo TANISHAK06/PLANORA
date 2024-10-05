b2687be467d7f04eee094ec952ed571dfa1736ca
TANISHAK06 <tanishakshukla08@gmail.com>
2024-10-06 03:05:50 +0530

    Initial Commit

---
+++ src/constants/options.jsx
@@ -0,0 +1,53 @@
+export const SelectTravelersList = [
+  {
+    id: 1,
+    title: "Just Me",
+    desc: "Forge your own path and embrace the freedom of solo adventures. Planora helps you craft a trip that’s all about you",
+    icon: "🧳",
+    people: "1 person",
+  },
+  {
+    id: 2,
+    title: "A couple",
+    desc: "Set the scene for unforgettable moments with your favorite person Planora makes planning the perfect getaway for two effortless",
+    icon: "💑",
+    people: "2 persons",
+  },
+  {
+    id: 3,
+    title: "Family",
+    desc: "With Planora, organize a fun-filled adventure tailored to every member of the family",
+    icon: "🚌",
+    people: "3 to 5 persons",
+  },
+  {
+    id: 4,
+    title: "Friends",
+    desc: "Planora makes group travel planning seamless and exciting for you and your friends.",
+    icon: "🚂",
+    people: "5 to 10 persons",
+  },
+];
+
+export const SelectBudgetOptions = [
+  {
+    id: 1,
+    title: "Cheap",
+    desc: "Stay conscious of costs",
+    icon: "🪙",
+  },
+  {
+    id: 2,
+    title: "Moderate",
+    desc: "Keep cost on the average side",
+    icon: "💰",
+  },
+  {
+    id: 3,
+    title: "Luxury",
+    desc: "Don't worry about cost",
+    icon: "💸",
+  },
+];
+
+export const AI_PROMPT = "Generate Travel Plan For Location :{location}";
